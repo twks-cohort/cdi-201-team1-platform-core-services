57b201e6c351ebf7285f41b815e7dacdfed56cce
Mark Sinclair <mark.sinclair@thoughtworks.com>
2023-07-12 16:22:04 -0500

    Force helm upgrade for prometheus stack deploy

--- scripts/deploy_prometheus.sh
+++ scripts/deploy_prometheus.sh
@@ -3875,4 +3875,4 @@ EOF
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
-helm upgrade --install prometheus prometheus-community/kube-prometheus-stack -n monitoring -f prometheus/values.yaml
+helm upgrade --force --install prometheus prometheus-community/kube-prometheus-stack -n monitoring -f prometheus/values.yaml
